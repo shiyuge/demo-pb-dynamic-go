cb06c66c9de22b572e791998a91c9b66abae49f7
shiyuge <shiyuge@outlook.com>
2022-07-15 21:19:18 +0800

    finish testfile

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,37 +1,73 @@
 package main
 
 import (
+	"io"
+	"log"
+	"os"
+
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"google.golang.org/protobuf/types/dynamicpb"
 )
 
 func main() {
-	name := "MyStruct"
-	var packet []byte
-	var pbFileProtoSet []byte
+	if len(os.Args) < 3 {
+		log.Fatalf("usage: first argument should be a compiled protobuf file descriptor set file")
+	}
+
+	descriptorFileSet, err := readFile(os.Args[1])
+	if err != nil {
+		log.Fatalf("fail to read descriptorFileSet: %+v", err)
+	}
+
+	incomingMessage, err := readFile(os.Args[2])
+	if err != nil {
+		log.Fatalf("fail to read incomingMessage: %+v", err)
+	}
+
 	var f descriptor.FileDescriptorSet
-	e := proto.Unmarshal(pbFileProtoSet, &f)
-	if e != nil {
-		// handle error
+	err = proto.Unmarshal(descriptorFileSet, &f)
+	if err != nil {
+		log.Fatalf("fail unmarshall FileDescriptorSet: %+v", err)
 	}
 
 	files := f.GetFile()
 	for _, file := range files {
+		log.Println("found file ", file.Name, " in file descriptor set")
 		messages := file.GetMessageType()
 		for _, m := range messages {
-			if m.GetName() == name {
-				printVal(m, packet)
+			log.Println("found message ", m.Name, " in file ", file.Name)
+			tryToParseIncomingMessageWithDescriptor(incomingMessage, m)
+			fields := m.GetField()
+			for _, field := range fields {
+				log.Println("found field ", field.Name, " in message ", m.Name, " in file ", file.Name)
 			}
 		}
 	}
 
 }
 
-func printVal(desc *descriptor.DescriptorProto, v []byte) {
-	m := dynamicpb.NewMessage(desc.ProtoReflect().Descriptor())
-	e := proto.Unmarshal(v, m)
-	if e != nil {
-		// handler error
+func readFile(fileName string) ([]byte, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
 	}
+
+	bs, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return bs, nil
+}
+
+func tryToParseIncomingMessageWithDescriptor(v []byte, d *descriptor.DescriptorProto) {
+	m := dynamicpb.NewMessage(d.ProtoReflect().Descriptor())
+	err := proto.Unmarshal(v, m)
+	if err != nil {
+		log.Println("cannot parse incoming message with message descriptor ", d.Name, " ", err.Error())
+		return
+	}
+
+	log.Println("message parsed successfully: ", m.String())
 }
